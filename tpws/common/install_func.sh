74aecf0a37c01f3130762fe4d12a48e4a5558c1a
Юрьев Александр <yurev_a@tochka.com>
2024-08-08 13:25:13 +0300

    Updated tpws

--- tpws/common/install_func.sh
+++ tpws/common/install_func.sh
@@ -105,23 +105,9 @@ config_copy_list_func() {
   fi
 }
 
-config_select_arch_func() {
-  if [ -z "$ARCH" ]; then
-    echo -e "\nSelect the router architecture: mipsel (default), mips, aarch64"
-    echo "  mipsel  - KN-1010/1011, KN-1810, KN-1910/1912, KN-2310, KN-2311, KN-2610, KN-2910, KN-3810"
-    echo "  mips    - KN-2410, KN-2510, KN-2010, KN-2012, KN-2110, KN-2112, KN-3610"
-    echo "  aarch64 - KN-2710, KN-1811"
-    read ARCH
-  fi
-
-  if [ "$ARCH" == "mips" ]; then
-    TPWS_URL="https://raw.githubusercontent.com/bol-van/zapret/master/binaries/mips32r1-msb/tpws"
-  elif [ "$ARCH" == "aarch64" ]; then
-    TPWS_URL="https://raw.githubusercontent.com/bol-van/zapret/master/binaries/aarch64/tpws"
-  else
-    ARCH="mipsel"
-    TPWS_URL="https://raw.githubusercontent.com/bol-van/zapret/master/binaries/mips32r1-lsb/tpws"
-  fi
+config_select_arch_func() {  
+  ARCH="mipsel"
+  TPWS_URL="https://raw.githubusercontent.com/bol-van/zapret/master/binaries/mips32r1-lsb/tpws"
 
   echo "Selected architecture: $ARCH"
   curl -SL# "$TPWS_URL" -o "$TPWS_BIN"
@@ -129,21 +115,10 @@ config_select_arch_func() {
 }
 
 config_select_mode_func() {
-  if [ -z "$MODE" ]; then
-    echo -e "\nSelect working mode: auto (default), list, all"
-    echo "  auto - automatically detects blocked resources and adds them to the list"
-    echo "  list - applies rules only to domains in the list $LISTFILE"
-    echo "  all  - applies rules to all traffic"
-    read MODE
-  fi
+  MODE = "list"
 
   if [ "$MODE" == "list" ]; then
     EXTRA_ARGS="--hostlist=$LISTFILE"
-  elif [ "$MODE" == "all" ]; then
-    EXTRA_ARGS=""
-  else
-    MODE="auto"
-    EXTRA_ARGS="--hostlist=$LISTFILE --hostlist-auto=$LISTAUTOFILE"
   fi
   echo "Selected mode: $MODE"
 
